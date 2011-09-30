ef8e27dfca9fabe51852311b19790cefb09657fe
Rob Meyer <rob@bigdis.com>
2011-09-29 18:30:31 -0700

    Express version actually now kinda works like a jukebox

--- express.js
+++ express.js
@@ -1,4 +1,32 @@
 var express = require('express');
+var util = require('util'),
+    exec = require('child_process').exec,
+    child;
+
+function runApplescriptFile(osascript, args, next) {
+    commandLine = 'osascript ' + osascript + " '" + args + "'";
+    exec(commandLine,
+		     function(error, stdout, stderr) {
+			 if (error !== null) {
+			     next('NodeError: ' + error + "\nargs:[" + commandLine + "]");
+			 } else {
+			     next(stdout);
+			 }
+		     });
+}
+
+function runApplescript(osascript, next) {
+    exec('osascript -e \'tell application "iTunes"\' -e "' + osascript + '" -e "end tell"',
+		     function (error, stdout, stderr) {
+			 if (error !== null) {
+			     next('NodeError: ' + error);
+			 } else {
+			     next(stdout);
+			 }
+		     });
+}
+
+
 
 var app = express.createServer();
 
@@ -25,11 +53,40 @@ app.get('/songs', function(req,res) {
     }});
 });
 
-app.get('/song/:id', function(req, res) {
-    debugger;
-    res.render('songdetail.jade', {locals: {
-	song:songs.find(req.params.id)
-    }});
+app.get('/playlist/:name', function(req,res) {
+    safe_playlist = req.params.name.replace(/[^A-Za-z-_0-9 \-]/g, "")
+    runApplescriptFile('listTracks.applescript', safe_playlist, function(data) {
+	songlist = [];
+	data.split('\n').forEach(function(line_item) {
+	    item = line_item.split('\\');
+	    songlist.push(
+		{id:item[0],
+			   name:item[1],
+			   album:item[2],
+			   artist:item[3],
+			   year:item[4],
+			   bitrate:item[5],
+			   runningtime:item[6]
+		});
+	});
+	res.render('songindex.jade', {locals: {
+	    songs:songlist
+	}});
+    });
+});
+
+app.get('/play/:id', function(req, res) {
+    safe_id = req.params.id.replace(/[^0-9]/g, "")
+    playstring = 'play (every track whose database ID is ' + safe_id + ')';
+    runApplescript(playstring, function(data) {
+	res.render('play.jade');
+    });
+});
+
+app.get('/stop', function(req, res) {
+    runApplescript('pause', function(data) {
+	res.render('play.jade');
+    });
 });
 	      
 
--- listTracks.applescript
+++ listTracks.applescript
@@ -3,7 +3,14 @@ on run argv
 	tell application "iTunes"
 		set mySongs to get (every track of playlist (item 1 of argv))
 		repeat with a_track in mySongs
-			set stdout to stdout & name of a_track & "
+			set stdout to stdout & (database ID of a_track) & "\\" & Â
+				(name of a_track) & "\\" & Â
+				(album of a_track) & "\\" & Â
+				(artist of a_track) & "\\" & Â				
+				(year of a_track) & "\\" & Â
+				(bit rate of a_track) & "\\" & Â
+				(time of a_track) & "\\" & Â
+				"
 "
 		end repeat
 	end tell

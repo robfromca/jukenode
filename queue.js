5ab197179a40c660e90506cfc320242ba70efa66
neil cronin <neilcronin@office-2.local>
2011-09-29 21:42:22 -0700

    different implementation backed by a data file.

--- queue.js
+++ queue.js
@@ -1,13 +1,30 @@
-var q = [];
+var readFile = require('fs').readFile;
+var writeFile = require('fs').writeFile;
 
-exports.add = function(itunesId) {
-	q.push(itunesId);
-};
+var queue=exports;
 
-exports.next = function() {
-	return q.shift();
-};
 
-exports.all = function() {
-	return q;
-};
+var sys = require("sys");
+
+var allSongs = [];
+
+readFile("./data/queue.txt", function(err, data) {  allSongs = JSON.parse(data); });
+
+queue.random = function() {
+	var song = allSongs[Math.floor(Math.random()*allSongs.length)];
+	return song;
+}
+
+queue.add = function(id) {
+	allSongs.push(id);
+	writeFile("data/queue.txt", JSON.stringify(allSongs));
+}
+
+queue.next = function() {
+	song = allSongs.shift();
+	writeFile("data/queue.txt", JSON.stringify(allSongs));
+}
+
+queue.contents = function() {
+	return allSongs;
+}
